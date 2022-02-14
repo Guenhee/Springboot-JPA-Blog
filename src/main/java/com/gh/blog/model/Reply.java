cec04275880208347a8ef103c6ba01bc9c71a9c7
GeunHee <sjkh1231@gmail.com>
2022-02-14 21:32:55 +0900

    JPA 연관관계 및 fetch
    JPA 연관관계 및 fetch

--- src/main/java/com/gh/blog/model/Reply.java
+++ src/main/java/com/gh/blog/model/Reply.java
@@ -1,5 +1,48 @@
 package com.gh.blog.model;
 
-public class Reply {
+import java.sql.Timestamp;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+//ORM -> Java(다른어어) Object -> 테이블로 매핑
+@Entity //클래스가 MySQL에 테이블이 생성된다.
+public class Reply {
+	
+	@Id //Primary key
+	@GeneratedValue(strategy = GenerationType.IDENTITY)//auto_increment
+	private int id;
+	
+	@Column(nullable = false , length  = 200)
+	private String content;
+	
+	@ManyToOne
+	@JoinColumn(name = "boardId")
+	private Board board;
+	
+	@ManyToOne
+	@JoinColumn(name = "userId")
+	private User user;
+	
+	@CreationTimestamp
+	private Timestamp createDate;
+	
+	
 }
--- src/main/java/com/gh/blog/model/User.java
+++ src/main/java/com/gh/blog/model/User.java
@@ -11,7 +11,17 @@ import javax.persistence.Id;
 import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 
-@Entity  //User클래스가 MySQL에 테이블이 생성된다.
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder 
+//ORM -> Java(다른어어) Object -> 테이블로 매핑
+@Entity //클래스가 MySQL에 테이블이 생성된다.
 public class User {
 	
 	@Id //Primary key
