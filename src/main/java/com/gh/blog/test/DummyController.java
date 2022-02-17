474ba7fd1d6871bb98ec1e8070d6193a241bf7b5
GeunHee <sjkh1231@gmail.com>
2022-02-17 21:50:23 +0900

    Transactional 더티 체킹
    Transactional 더티 체킹

--- src/main/java/com/gh/blog/test/DummyController.java
+++ src/main/java/com/gh/blog/test/DummyController.java
@@ -1,11 +1,20 @@
 package com.gh.blog.test;
 
+import java.util.List;
 import java.util.function.Supplier;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gh.blog.model.RoleType;
@@ -18,6 +27,38 @@ public class DummyController {
 	
 	@Autowired //의존성 주입(DI)
 	private UserRepository userRepository;
+	
+	@Transactional
+	@PutMapping("/dummy/user/{id}")
+	public User updateUser(@PathVariable int id, @RequestBody User requestUser) {
+		System.out.println("id : "+id);
+		System.out.println("password : "+requestUser.getPassword());
+		System.out.println("email : "+requestUser.getEmail());
+		
+		User user = userRepository.findById(id).orElseThrow(()->{
+			return new IllegalArgumentException("수정에 실패하였습니다.");
+		});
+		user.setPassword(requestUser.getPassword());
+		user.setEmail(requestUser.getEmail());
+		
+		//userRepository.save(requestUser);
+		
+		//더티 체킹
+		return null;
+	}
+	
+	
+	@GetMapping("/dummy/users")
+	public List<User> list(){
+		return userRepository.findAll();
+	}
+	
+	//한 페이지당 2건의 데이터를 리턴받아 볼 예정
+	@GetMapping("/dummy/user")
+	public List<User> pageList(@PageableDefault (size=2 , sort= "id",direction = Sort.Direction.DESC)Pageable pageable){
+		List<User> users = userRepository.findAll(pageable).getContent();
+		return users;
+	}
 	
 	//{id}주소로 파라미터를 전달 받을 수 있음.
 	@GetMapping("/dummy/user/{id}")
